63d3029e35ee1c89504bf2e65d0f0a38ecef6bc1
Andras Gall <a.gall@hvg.hu>
2020-04-20 09:53:25 +0200

    na

---
+++ Controllers/TrendingArticlesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace trending_query_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrendingArticlesController : ControllerBase
+    {
+        private readonly ILogger<TrendingArticlesController> _logger;
+
+        public TrendingArticlesController(ILogger<TrendingArticlesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<Guid> Get()
+        {
+            return new Guid[] {
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            };
+        }
+    }
+}
